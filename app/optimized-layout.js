668ed62d40ff26c463142d22854972ba41c465b0
Khayyis Billawal <khayyis.billawal@gmail.com>
2025-12-22 10:38:41 +0700

    feat: add ClickSpark component from React Bits for click spark animation effect

--- app/optimized-layout.js
+++ app/optimized-layout.js
@@ -3,23 +3,33 @@
 import ThemeProvider from '../components/ThemeProvider';
 import SplashCursor from '../components/SplashCursor';
 import OptimizedTransitionLayout from '../components/OptimizedTransitionLayout';
+import ClickSpark from '../components/ClickSpark';
 
 /**
  * OptimizedLayout - Layout yang dioptimalkan untuk performa
  * 
- * Komponen ini menggabungkan ThemeProvider, SplashCursor, dan OptimizedTransitionLayout
+ * Komponen ini menggabungkan ThemeProvider, SplashCursor, ClickSpark, dan OptimizedTransitionLayout
  * untuk memberikan pengalaman pengguna yang optimal dengan mempertimbangkan
  * kecepatan koneksi dan kemampuan perangkat.
  * 
  * Menggunakan ElegantLoading untuk animasi loading yang elegan dengan CSS murni.
+ * ClickSpark memberikan efek spark animasi saat user mengklik di mana saja.
  */
 export default function OptimizedLayout({ children }) {
   return (
     <ThemeProvider>
-      <SplashCursor />
-      <OptimizedTransitionLayout>
-        {children}
-      </OptimizedTransitionLayout>
+      <ClickSpark
+        sparkColor="#6366f1"
+        sparkSize={12}
+        sparkRadius={20}
+        sparkCount={10}
+        duration={500}
+      >
+        <SplashCursor />
+        <OptimizedTransitionLayout>
+          {children}
+        </OptimizedTransitionLayout>
+      </ClickSpark>
     </ThemeProvider>
   );
 }
